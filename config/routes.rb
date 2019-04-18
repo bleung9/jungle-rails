bb864c5bacee8be44bfc09dd4467492f872d68f2
Bosco <bleung9@gmail.com>
2019-04-18 20:28:40 +0000

    implementd user logins and registrations

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,19 @@ Rails.application.routes.draw do
 
   resources :orders, only: [:create, :show]
 
+  resources :users, only: [:create, :new]
+
+  # resources :sessions, only: [:new, :create, :destroy]
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
+  
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
